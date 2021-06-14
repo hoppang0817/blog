ae3789256400b613b469dc6cd707927faffbcf7e
hoppang0817 <62546979+hoppang0817@users.noreply.github.com>
2021-06-14 17:31:31 +0900

    delete 이해

--- blog01/src/main/java/com/bit/test/DummyControllerTest.java
+++ blog01/src/main/java/com/bit/test/DummyControllerTest.java
@@ -3,16 +3,20 @@ package com.bit.test;
 import java.util.List;
 import java.util.function.Supplier;
 
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bit.blog.repository.UserRepository;
@@ -25,7 +29,45 @@ public class DummyControllerTest {
 	@Autowired //의존성 주입
 	public UserRepository userRepository;
 
+	@DeleteMapping("/dummy/user/{id}")
+	public String deleteUser(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다 .해당 아이디는 db에없습니다";
+		}
+		return "삭제되었습니다 " + id;
+		
+	}
 	
+	//email,password 수정
+	//@RequestBody -> json 타입으로 받을려면 사용해야함
+	//save 함수는 id 를 전달하지 않으면 insert를 해주고
+	//save 함수는 id 를 전달하면 해당 id 에 대한 데이터가 있으면 update를 해주고
+	//save 함수는 id 를 전달하면 해당 id 에 대한 데이터가 없으면 insert를 해준다
+	
+	@Transactional //메소드가 끝나며 자동 commit됨
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id , @RequestBody User requestUser) {//json데이터를 요청 -> Java Object(MessageConverter의 Jackson라이브러리가 변환해서 받아와줌)
+		System.out.println("id : "+id);
+		System.out.println("password : "+ requestUser.getPassword());
+		System.out.println("email : "+ requestUser.getEmail());
+		
+		//유저 id로 유저 정보를 가지고와 user에 담는다
+		//이때 영속화 됨
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정 실패");
+		});
+		//user에 담겨있는 정보를 변경한다
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		//requestUser.setId(id);
+		//변경된 정보를 save
+		//userRepository.save(user);
+
+		return user;
+		
+	}
 	
 	//전체 데이터를 받아오는거라 특정 파라메터 없어도됨
 	@GetMapping("/dummy/users")
@@ -69,6 +111,7 @@ public class DummyControllerTest {
 		// user 오브젝트를 json으로 변환해서 브라우저에게 던져줍니다.
 		return user;
 	}
+	
 	
 //	@PostMapping("/dummy/join")
 //	public String join(String username, String password,String email) {//key = value(약속된 교칙)
